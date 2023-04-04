346ac5947e9584c6aef43ae220f0d9f64b3fd8f7
Suyash Singh <112690036+suyashsingh166@users.noreply.github.com>
2023-04-04 15:04:48 +0530

    Update Email.java

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -5,6 +5,9 @@ public class Email {
     private String emailId;
     private String password;
 
+    public Email() {
+    }
+
     public Email(String emailId){
         this.emailId = emailId;
         this.password = "Accio@123";
@@ -25,5 +28,38 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(getPassword().equals(oldPassword)){
+            if(isValid(newPassword)){
+                password=newPassword;
+            }
+        }
+    }
+    public boolean isValid(String password){
+        Boolean cap=false;
+        Boolean small=false;
+        Boolean digit=false;
+        Boolean special=false;
+        if(password.length()<8){
+            return false;
+        }
+        for(int i=0;i<password.length();i++){
+            char ch=password.charAt(i);
+            if(ch>='A' && ch<='Z'){
+                cap=true;
+            }
+            else if(ch>='a' && ch<='z'){
+                small=true;
+            }
+            else if(ch>='0' && ch<='9'){
+                digit=true;
+            }
+            else{
+                special=true;
+            }
+        }
+        if(cap && small && digit && special){
+            return true;
+        }
+        return false;
     }
 }
